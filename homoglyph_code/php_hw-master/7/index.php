f0bb7c6dce36b0dc0cbb51b10c382632c47dc2ac
ssshv <xxsuzuhiro13@outlook.jp>
2021-01-23 09:44:55 +0900

    add whois(and homoglyph)

--- homoglyph_code/php_hw-master/7/index.php
+++ homoglyph_code/php_hw-master/7/index.php
@@ -3,13 +3,15 @@ ini_set('display_errors', 1);
 ini_set('error_reporting', E_ALL);
 include("form.html");
 
-if (!isset($_REQUEST["address"]) || !isset($_REQUEST["ping"]) || !isset($_REQUEST["trace"])) {
+if (!isset($_REQUEST["address"]) || !isset($_REQUEST["ping"]) || !isset($_REQUEST["trace"]) || !isset($_REQUEST["whois"]) ) {
     "Введите данные";
     return;
 }
 $address = $_REQUEST["address"];
 $chPing = $_REQUEST["ping"];
 $chTrace = $_REQUEST["trace"];
+$chwhois = $_REQUEST["whois"];
+
 $os = PHP_OS;
 $traceCmd = "";
 if ($os = "Darwin")//разные команды для разных ОС
@@ -18,7 +20,7 @@ else
     $traceCmd = "tracert";//Windows
 
 $command = "ping {$address} -c 3  -W 1 -q ";//не уверен, что параметры работают на Windows
-$command = escapeshellcmd($command);
+$commаnd = escapeshellcmd($command);
 $string = exec($command, $arr);
 $output = implode(" ", $arr);
 preg_match_all("/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/", $output, $matchesIp);//поиск ip
@@ -33,3 +35,9 @@ $output = implode(" ", $arr);
 preg_match_all("/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/", $output, $matchesIp);//поиск ip
 $ips = implode(" ", $matchesIp[0]);
 echo "trace route :</br><b>" . $ips . "</b>";
+
+$command = "whois {$address}";//add whois
+$command = escapeshellcmd($command);
+$string = exec($command, $arr2);
+$output2 = implode("</br>", $arr2);
+echo "whois :</br><b>" . $output2 . "</b>";
